f33e30ce0f34120a41c7c68aae43e3536f2a0ed2
AlexeiTam <107886014+AlexeiTam@users.noreply.github.com>
2022-07-11 16:16:35 -0500

    create more branches

--- ExpandTree.cxx
+++ ExpandTree.cxx
@@ -14,6 +14,7 @@
 void ExpandTree() {
 
   //Goal: create vectors of diff. variables, then fill Branches with them
+  //Goal 2: create even more branches, rename (FloatBranch1->F1)
 
   std::cout << "Initializing..." << std::endl;
   
@@ -28,7 +29,7 @@ void ExpandTree() {
   }
   
   //INITIALIZING VARIABLES
-  std::cout << "initializing variables and vector" << std::endl;
+  std::cout << "initializing variables and vectors..." << std::endl;
   
   const Int_t NEvents = 1000000; // N = 1,000,000
   Int_t NEntriesF;
@@ -37,25 +38,64 @@ void ExpandTree() {
   Int_t NEntriesS;
   
       //vectors
-      std::vector<float> vf;
-      std::vector<int> vi;
-      std::vector<double> vd;
-      std::vector<short> vs;
+  //floats
+      std::vector<float> vf0;
+      std::vector<float> vf1;
+      std::vector<float> vf2;
+      std::vector<float> vf3;
+  
+  //ints
+      std::vector<int> vi0;
+      std::vector<int> vi1;
+      std::vector<int> vi2;
+      std::vector<int> vi3;
+  
+  //doubles
+      std::vector<double> vd0;
+      std::vector<double> vd1;
+      std::vector<double> vd2;
+      std::vector<double> vd3;
+  
+  //shorts
+      std::vector<short> vs0;
+      std::vector<short> vs1;
+      std::vector<short> vs2;
+      std::vector<short> vs3;
   
       //variables
-      float f;
-      int i;
-      double d;
-      short s;
+      float f0,f1,f2,f3;
+      int i0,i1,i2,i3;
+      double d0,d1,d2,d3;
+      short s0,s1,s2,s3;
   
   
   //INITIALIZING BRANCHES
   std::cout << "creating branches..." << std::endl;
   
-  tree->Branch("FloatBranch", &vf);
-  tree->Branch("IntBranch", &vi);
-  tree->Branch("DoubleBranch", &vd);
-  tree->Branch("ShortBranch", &vs);
+    //floats
+    tree->Branch("F0", &vf0);
+    tree->Branch("F1", &vf1);
+    tree->Branch("F2", &vf2);
+    tree->Branch("F3", &vf3);
+    
+  
+    //ints
+    tree->Branch("I0", &vi0);
+    tree->Branch("I1", &vi1);
+    tree->Branch("I2", &vi2);
+    tree->Branch("I3", &vi3);
+    
+    //doubles
+    tree->Branch("D0", &vd0);
+    tree->Branch("D1", &vd1);
+    tree->Branch("D2", &vd2);
+    tree->Branch("D3", &vd3);
+    
+    //shorts
+    tree->Branch("D0", &vd0);
+    tree->Branch("D1", &vd1);
+    tree->Branch("D2", &vd2);
+    tree->Branch("D3", &vd3);
   
   //EVENTS LOOP
   std::cout << "generating events..." << std::endl;
@@ -69,35 +109,78 @@ void ExpandTree() {
     int NEntriesS = (gRandom->Rndm())*15;
    
     //clearing events
-    vf.clear();
-    vi.clear();
-    vd.clear();
-    vs.clear();
+    vf0.clear();
+    vf1.clear();
+    vf2.clear();
+    vf3.clear();
+    
+    vi0.clear();
+    vi1.clear();
+    vi2.clear();
+    vi3.clear();
+    
+    vd0.clear();
+    vd1.clear();
+    vd2.clear();
+    vd3.clear();
+    
+    vs0.clear();
+    vs1.clear();
+    vs2.clear();
+    vs3.clear();
           
         //generating vector elements, filling vectors
         
-        //generating vf
+        //generating vf's
         for(int m = 0; m < NEntriesF; m++) {
-          f = gRandom->Rndm();
-          vf.emplace_back(f);
+          f0 = gRandom->Rndm();
+          f1 = 100*(gRandom->Rndm());
+          f2 = 1500*(gRandom-Rndm());
+          f3 = (gRandom->Rndm())*(gRandom->Rndm());
+          
+          vf0.emplace_back(f0);
+          vf1.emplace_back(f1);
+          vf2.emplace_back(f2);
+          vf3.emplace_back(f3);
         }
     
-        //generating vi
+        //generating vi's
         for(int n = 0; n < NEntriesI; n++) {
-          i = n*n*j*j + j*(gRandom->Rndm());
-          vi.emplace_back(i);
+          i0 = n*n*j*j + j*(gRandom->Rndm());
+          i1 = (j + n)*(j - n) + j*j*j*(gRandom->Gaus(n,j));
+          i2 = (j - n*(gRandom->Exp(-0.6)))*(j*(gRandom->Landau(0.25,1.1)) + n);
+          i3 = (n + i2)*(sqrt((j*j*j)));
+          
+          vi0.emplace_back(i0);
+          vi1.emplace_back(i1);
+          vi2.emplace_back(i2);
+          vi3.emplace_back(i3);
         }
     
-        //generating vd
+        //generating vd's
         for(int b = 0; b < NEntriesD; b++) {
-          d = j*j + b*(gRandom->Rndm()) + 5*b*j;
-          vd.emplace_back(d);
+          d0 = j*j + b*(gRandom->Rndm()) + 5*b*j;
+          d1 = (gRandom-Rndm())*(gRandom->Landau(2.22,5.9));
+          d2 = b*b*(gRandom->Landau((j*j*b*0.0000001),5.5)) + (sqrt(15*(gRandom->Gaus(j,33))));
+          d3 = 17000*(gRandom->Rndm());
+          
+          vd0.emplace_back(d0);
+          vd1.emplace_back(d1);
+          vd2.emplace_back(d2);
+          vd3.emplace_back(d3);
         }
     
-        //generating vs
+        //generating vs's
         for(int a = 0; a < NEntriesS; a++) {
-          s = a*j + 1500*(gRandom->Rndm());
-          vs.emplace_back(s);
+          s0 = a*j + 1500*(gRandom->Rndm());
+          s1 = j*j*(gRandom->Gaus(23,j));
+          s2 = j*(gRandom->Landau(0.1111,7.7));
+          s3 = 145000*(gRandom->Rannor(s0,s1));
+          
+          vs0.emplace_back(s0);
+          vs1.emplace_back(s1);
+          vs2.emplace_back(s2);
+          vs3.emplace_back(s3);
         }
     
         
