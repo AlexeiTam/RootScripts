8551fd781c08222a0d95cd5c97bab88987980567
AlexeiTam <107886014+AlexeiTam@users.noreply.github.com>
2022-07-18 16:05:07 -0500

    minor fix

--- AutoFlushTestSmall.cxx
+++ AutoFlushTestSmall.cxx
@@ -113,7 +113,7 @@ std::cout << "initializing branches..." << std::endl;
 		for(int m = 0; m < NB; m++) {
 		
 		fb0 = gRandom->Rndm();
-		fb1 = gRandom->Gaus(0,1);
+		fb1 = gRandom->Poisson(23.12);
 			
 		vb0.emplace_back(fb0);
 		vb1.emplace_back(fb1);
