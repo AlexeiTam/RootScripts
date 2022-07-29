8717d76a17e07005d92eda38fba52c47b24505fe
AlexeiTam <107886014+AlexeiTam@users.noreply.github.com>
2022-07-29 13:49:12 -0500

    test Serhan's old code

--- LargeAutoFlushTest.cxx
+++ LargeAutoFlushTest.cxx
@@ -43,6 +43,11 @@ void LargeAutoFlushTest(Long64_t NewAutoF = -30000000) {
 	//h0->GetXaxis()->SetTitle("Entry Value");
 	//h0->GetYaxis()->SetTitle("Number of Entries");
 	
+	h1->GetYaxis()->SetTitle("YLabel"); 
+  	h1->GetYaxis()->SetTitleSize(0.05);
+  	h1->GetYaxis()->SetTitleOffset(1.2);
+  	h1->GetYaxis()->SetLabelSize(0.04);
+	
 	//h1->GetXaxis()->SetTitle("Entry Value");
 	//h1->GetYaxis()->SetTitle("Number of Entries");
 	
